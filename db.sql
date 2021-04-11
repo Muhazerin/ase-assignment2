c5ea9cb6db911f61927cd2c456fbc6e4b4117797
Muhazerin <muhd.muhazerin@gmail.com>
2021-04-11 19:03:14 +0800

    database script

---
+++ db.sql
@@ -0,0 +1,11 @@
+create database if not exists ase;
+
+use ase;
+
+create table users (
+	userName varchar (50),
+	password varchar (50));
+
+insert into users values('Muhazerin', 'password');
+insert into users values('Ray', 'password');
+insert into users values('Lusi', 'password');
